a84422c16daa403a38750bc8c89301afb2a8f1c1
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-07-10 08:51:33 -0700

    ex2 done, not sure if linear though

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,33 @@ class Ticket:
         self.source = source
         self.destination = destination
 
-
+    
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
+
+    # tickets = list of Tickets
+    # length = length of tickets list
+
+    ticketsDictionary = {}
+    route = [None] * length
+
+    # fill dict with tickets
+    for ticket in tickets:
+        print(ticket.source, ticket.destination)
+        ticketsDictionary[ticket.source] = ticket.destination
+        # first one should be source = "NONE", last is "NONE" for destination
+        if ticket.source == "NONE":
+            route[0] = ticket.destination
+    
+    i = 1
+    # stop when the route list's last item is not None
+    while route[length - 1] == None:
+        route[i] = ticketsDictionary[route[i-1]]
+        i += 1
+        
+    print(ticketsDictionary)
 
     return route
