aea23044adeac9b1012796af06cc10f8c763e128
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-07-10 09:36:54 -0700

    ex1 done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -14,45 +14,48 @@ def get_indices_of_item_weights(weights, length, limit):
     table = {}
 
     # fill up table dict with weight as key and limit - weight as value
-    print("Filling in dictionary")
+    # OR just the index as the key, and the weight as a value
+    # print("Filling in dictionary")
+
     for i in range(length):
-        print(weights[i])
-        table[weights[i]] = i
+        # print(f"key = {i} value = {weights[i]}")
+        # table[weights[i]] = i
+        table[i] = weights[i]
 
     high = None
     low = None
     matches = []
     
-    print(f"table starts = {table}")
+    # print(f"table starts = {table}")
     for key in table:
         # if value is equal to a KEY in the table dict
-        if limit - key in table:
-            print(f"match found, {table[key]}")
-            matches.append(table[key])
+        # OR if 
+        if limit - table[key] in table.values():
+            # print(f"match found, {table[key]}")
+            matches.append(key)
             # print(table[key])
     
-    print(f"matches = {matches}")
-    print(f"table = {table}")
+    # print(f"matches = {matches}")
+    # print(f"table = {table}")
 
     matches.sort()
+    # now [low, high]
     if len(matches) == 2:
         high = matches[1]
         low = matches[0]
-        return (high, low)
+        return [high, low]
     
     
     if high == None and low == None:
-        print("NO PAIR FOUND")
+        # print("NO PAIR FOUND")
         return None
     
+# print("\n---START---")
+# print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
 
-
-print("\n---START---")
-print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
-
-    #         weights_2 = [4, 4]
-    #         answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
-    #         self.assertTrue(answer_2[0] == 1)
-    #         self.assertTrue(answer_2[1] == 0)
-print("2nd\n")
-print(get_indices_of_item_weights([4, 4], 2, 8))
+#     #         weights_2 = [4, 4]
+#     #         answer_2 = get_indices_of_item_weights(weights_2, 2, 8)
+#     #         self.assertTrue(answer_2[0] == 1)
+#     #         self.assertTrue(answer_2[1] == 0)
+# print("2nd\n")
+# print(get_indices_of_item_weights([4, 4], 2, 8))
